29a610b309522072db76499e11030a1a15542f82
Shayna <shaynak112@gmail.com>
2017-05-27 21:19:10 -0400

    Add files via upload

---
+++ createAccountWidget.php
@@ -0,0 +1,7 @@
+            <div class="col-md-4">
+                <aside class="widget news-letter">
+                    <h3 class="widget-title text-uppercase">Create an account.</h3>
+                    <p>Create an account to interact with other technology enthusiasts and post your own articles.</p>
+                    <a href="http://nathanmante.com/News/Users/createaccount.php" id="link">Create an account.</a>
+                </aside>
+            </div>
